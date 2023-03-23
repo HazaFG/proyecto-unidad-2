de02f16a6bbcb31c285fb10a3893313c1c93dafa
Haza Pro <hazaelfg@gmail.com>
2023-03-22 22:36:10 -0700

    Initial commit

---
+++ src/Ventana.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+    public Ventana(){
+        this.setVisible(true);
+        this.setSize(600,600);
+        this.setTitle("HOLA MI AMOR");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
