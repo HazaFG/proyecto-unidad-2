dcea9308d4f634c1112a526abe6efbc2a474f86d
Andrea Trasviña <alucero_21@alu.uabcs.mx>
2023-03-22 22:52:46 -0700

    prueba

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Ventana extends JFrame {
     public Ventana(){
         this.setVisible(true);
         this.setSize(600,600);
-        this.setTitle("HOLA MI AMOR");
+        this.setTitle("a");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setBackground(Color.RED);
     }
 }
