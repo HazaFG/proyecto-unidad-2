a88a4cf094654148d823b57aa60b26a6987f850c
Andrea Trasviña <alucero_21@alu.uabcs.mx>
2023-03-22 23:32:16 -0700

    tercera prueba

--- src/Ventana.java
+++ src/Ventana.java
@@ -5,12 +5,14 @@ public class Ventana extends JFrame {
     public Ventana(){
         this.setVisible(true);
         this.setSize(600,600);
-        this.setTitle("a");
+        this.setTitle("HOLA");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setBackground(Color.RED);
 
         JPanel fondo = new JPanel();
         fondo.setOpaque(true);
         this.setBackground(Color.RED);
+
+        this.add(fondo);
     }
 }
