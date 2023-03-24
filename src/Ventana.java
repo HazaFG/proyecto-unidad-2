4795d84afdc3fd3655ffcc7a29c24786b1b1a25e
Andrea Trasviña <alucero_21@alu.uabcs.mx>
2023-03-23 20:31:27 -0700

    Primera Actualizacion: Estructura

--- src/Ventana.java
+++ src/Ventana.java
@@ -4,9 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Ventana extends JFrame {
-    private String anterior = "ola";
-    private String actual = "login";
-    public JPanel panel = null;
+    private JPanel panelActual;
+    private JPanel panelAnterior;
+
     public Ventana() {
         //PROPIEDADES VENTANA
         this.setTitle("Gestión de Usuarios");
@@ -18,204 +18,226 @@ public class Ventana extends JFrame {
         this.setBackground(Color.RED);
         this.setVisible(true);
 
-        limpiarVentana();
-       /*//PANEL PRINCIPAL
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
-        setContentPane(panel);*/
+        cambiarPanel(acceder());
 
         this.repaint();
         this.revalidate();
     }
-    public void limpiarVentana() {
-        if(panel!= null) {
-            this.remove(panel);
-        }
 
-        if(actual.equals("login")){
-            panel = login();
+    private void cambiarPanel(JPanel nuevoPanel) {
+        panelAnterior = panelActual;
+        panelActual = nuevoPanel;
 
-            this.add(panel);
-
-            this.repaint();
-            this.revalidate();
-        }
-
-        if(actual.equals("registro")){
-            panel = registro();
-
-            this.add(panel);
-
-            this.repaint();
-            this.revalidate();
-        }
-
+        limpiarVentana();
     }
 
-    public JPanel login()
-    {
+    private void limpiarVentana() {
+        if (panelAnterior != null) {
+            this.remove(panelAnterior);
+        }
 
-        anterior = actual;
-        actual = "login";
+        this.add(panelActual);
 
-        JPanel jp1 = new JPanel();
-        jp1.setSize(600, 600);
-        jp1.setLocation(0, 0);
-        jp1.setLayout(null);
-        jp1.setBackground(Color.decode("#5ED6DF"));
+        this.repaint();
+        this.revalidate();
+    }
 
-        //titulo
-        JLabel title = new JLabel("Registro nuevo usuario",JLabel.CENTER);
-        title.setFont(new Font("Comic Sans", Font.BOLD,20));
-        title.setSize(280, 40);
-        title.setLocation(100, 20);
-        title.setOpaque(true);
-        title.setBackground(Color.green);
-        jp1.add(title);
+    //SPLASH
 
-        JLabel tag1 = new JLabel("Ingrese el nombre de usuario: ",JLabel.CENTER);
-        tag1.setSize(250, 20);
-        tag1.setLocation(120, 80);
-        tag1.setOpaque(true);
-        tag1.setBackground(Color.black);
-        tag1.setForeground(Color.white);
-        jp1.add(tag1);
 
-        JTextField username = new JTextField("");
-        username.setSize(250, 40);
-        username.setLocation(120, 100);
-        jp1.add(username);
 
-        JLabel tag2 = new JLabel("Ingrese la contraseña: ",JLabel.CENTER);
-        tag2.setSize(250, 20);
-        tag2.setLocation(120, 140);
-        tag2.setOpaque(true);
-        tag2.setBackground(Color.black);
-        tag2.setForeground(Color.white);
-        jp1.add(tag2);
-
-        JPasswordField password = new JPasswordField();
-        password.setSize(250, 40);
-        password.setLocation(120, 160);
-        jp1.add(password);
+    //ACCEDER CUENTA
+    public JPanel acceder() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.decode("#5ED6DF"));
 
         JButton btnAccess = new JButton("A C C E D E R");
         btnAccess.setSize(250,40);
         btnAccess.setLocation(120,205);
-        jp1.add(btnAccess);
+        panel.add(btnAccess);
 
         btnAccess.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-
-                anterior = actual;
-                actual = "registro";
-
-                limpiarVentana();
-
+                cambiarPanel(cuenta());
             }
 
         });
 
-        this.add(jp1);
-        return jp1;
-
+        return panel;
     }
 
-    public JPanel registro() {
+    //CUENTA
+    public JPanel cuenta() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.decode("#C45EDF"));
 
-        anterior = actual;
-        actual = "registro";
-
-        JPanel jp2 = new JPanel();
-        jp2.setSize(600, 600);
-        jp2.setLocation(0, 0);
-        jp2.setLayout(null);
-        jp2.setBackground(Color.decode("#C45EDF"));
-
-        //titulo
-        JLabel titleR = new JLabel("Registro nuevo usuario",JLabel.CENTER);
-        titleR.setFont(new Font("Comic Sans", Font.BOLD,20));
-        titleR.setSize(280, 40);
-        titleR.setLocation(100, 20);
-        titleR.setOpaque(true);
-        titleR.setBackground(Color.green);
-        jp2.add(titleR);
-
-        JLabel tag1R = new JLabel("Ingrese el nombre de usuario: ",JLabel.CENTER);
-        tag1R.setSize(250, 20);
-        tag1R.setLocation(120, 80);
-        tag1R.setOpaque(true);
-        tag1R.setBackground(Color.black);
-        tag1R.setForeground(Color.white);
-        jp2.add(tag1R);
-
-        JTextField usernameR = new JTextField("");
-        usernameR.setSize(250, 40);
-        usernameR.setLocation(120, 100);
-        jp2.add(usernameR);
-
-        JLabel tag2R = new JLabel("Ingrese el correo electrónico: ",JLabel.CENTER);
-        tag2R.setSize(250, 20);
-        tag2R.setLocation(120, 140);
-        tag2R.setOpaque(true);
-        tag2R.setBackground(Color.black);
-        tag2R.setForeground(Color.white);
-        jp2.add(tag2R);
-
-        JPasswordField passwordR = new JPasswordField();
-        passwordR.setSize(250, 40);
-        passwordR.setLocation(120, 160);
-        jp2.add(passwordR);
-
-        JLabel tag3 = new JLabel("Ingrese el nombre: ",JLabel.CENTER);
-        tag3.setSize(250, 20);
-        tag3.setLocation(120, 210);
-        tag3.setOpaque(true);
-        tag3.setBackground(Color.black);
-        tag3.setForeground(Color.white);
-        jp2.add(tag3);
-
-        JTextField name = new JTextField("");
-        name.setSize(250, 40);
-        name.setLocation(120, 250);
-        jp2.add(name);
-
-        JLabel tag4 = new JLabel("Ingrese el nombre: ",JLabel.CENTER);
-        tag4.setSize(250, 20);
-        tag4.setLocation(120, 300);
-        tag4.setOpaque(true);
-        tag4.setBackground(Color.black);
-        tag4.setForeground(Color.white);
-        jp2.add(tag4);
-
-        JTextField lastname = new JTextField("");
-        lastname.setSize(250, 40);
-        lastname.setLocation(120, 340);
-        jp2.add(lastname);
-
-        JButton btnAdd = new JButton("R E G I S T R O");
+        JButton btnAdd = new JButton("tercera ventana");
         btnAdd.setSize(250,40);
         btnAdd.setLocation(120, 390);
-        jp2.add(btnAdd);
+        panel.add(btnAdd);
 
         btnAdd.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-
-                anterior = actual;
-                actual = "login";
-
-                limpiarVentana();
-
+                cambiarPanel(modificarCuenta());
             }
 
         });
 
-        return jp2;
+        return panel;
+    }
+
+    //MODIFICAR CUENTA PERSONAL
+    public JPanel modificarCuenta() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.decode("#eeeeee"));
+
+        JButton btnAdd = new JButton("cuarta ventana");
+        btnAdd.setSize(250,40);
+        btnAdd.setLocation(120, 390);
+        panel.add(btnAdd);
+
+        btnAdd.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarPanel(cerrarSesion());
+            }
+
+        });
+
+        return panel;
+    }
+
+    //CERRAR SESIÓN
+    public JPanel cerrarSesion() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.green);
+
+        JButton btnAdd = new JButton("quinta ventana");
+        btnAdd.setSize(250,40);
+        btnAdd.setLocation(120, 390);
+        panel.add(btnAdd);
+
+        btnAdd.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarPanel(listaUsuarios());
+            }
+
+        });
+
+        return panel;
+    }
+
+    //LISTA DE USUARIOS
+    public JPanel listaUsuarios() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.red);
+
+        JButton btnAdd = new JButton("6 ventana");
+        btnAdd.setSize(250,40);
+        btnAdd.setLocation(120, 390);
+        panel.add(btnAdd);
+
+        btnAdd.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarPanel(borrarUsuarios());
+            }
+
+        });
+        return panel;
+    }
+    //BORRAR USUARIOS
+    public JPanel borrarUsuarios() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.PINK);
+
+        JButton btnAdd = new JButton("borrar");
+        btnAdd.setSize(250,40);
+        btnAdd.setLocation(120, 390);
+        panel.add(btnAdd);
+
+        btnAdd.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarPanel(crearUsuario());
+            }
+
+        });
+        return panel;
+    }
+
+    //CREAR USUARIO
+    public JPanel crearUsuario() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.BLUE);
+
+        JButton btnAdd = new JButton("crear usuario");
+        btnAdd.setSize(250,40);
+        btnAdd.setLocation(120, 390);
+        panel.add(btnAdd);
+
+        btnAdd.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarPanel(comoCrearUsuario());
+            }
+
+        });
+        return panel;
+    }
+
+    //COMO CREAR USUARIO
+    public JPanel comoCrearUsuario() {
+        JPanel panel = new JPanel();
+        panel.setSize(600, 600);
+        panel.setLocation(0, 0);
+        panel.setLayout(null);
+        panel.setBackground(Color.orange);
+
+        JButton btnAdd = new JButton("ayuda");
+        btnAdd.setSize(250,40);
+        btnAdd.setLocation(120, 390);
+        panel.add(btnAdd);
+
+        btnAdd.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarPanel(crearUsuario());
+            }
+
+        });
+        return panel;
     }
 }
